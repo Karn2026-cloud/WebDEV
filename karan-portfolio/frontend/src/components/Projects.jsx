5a2b5c6c4467ddb182fb8d325f5d438398171368
Karn2026-cloud <karanindrale253@gmail.com>
2026-06-25 15:05:12 +0530

    Update Projects.jsx

--- karan-portfolio/frontend/src/components/Projects.jsx
+++ karan-portfolio/frontend/src/components/Projects.jsx
@@ -17,7 +17,7 @@ const Projects = () => {
     try {
 
       const res = await axios.get(
-        "https://karan-portfolio-backend.onrender.com/api/projects"
+        "https://webdev-1-jsp7.onrender.com/api/projects"
       );
 
       setProjects(res.data);
